af392f2cd5f687c7a887b7ccc0491b16fff15deb
ObertiFabio <101709153+ObertiFabio@users.noreply.github.com>
2023-05-18 11:57:46 +0000

    unit test

--- registro/src/test/java/it/paleocapa/obertif/AppTest.java
+++ registro/src/test/java/it/paleocapa/obertif/AppTest.java
@@ -4,15 +4,78 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * Unit test for simple App.
- */
+
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
+    Registro r = new Registro();
+    Registro r1 = new Registro();
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    void registraValutazione() {
+        r.registraValutazione("123", 8, "storia");
+        assertEquals(1, r.registroElettronico.get("123").size());
     }
+
+    @Test
+    void calcoloMediaAlunno() {
+
+        r.registraValutazione("123", 8, "storia");
+        r.registraValutazione("123", 6, "storia");
+        assertEquals(7, r.calcoloMediaAlunno("123"));
+    }
+
+    @Test
+    void mediaMateria() {
+
+        r.registraValutazione("123", 8, "storia");
+        r.registraValutazione("123", 6, "storia");
+        r.registraValutazione("123", 7, "storia");
+        r.registraValutazione("123", 6, "geografia");
+        assertEquals(7, r.mediaMateria("123", "storia"));
+    }
+    /* 
+    @Test
+    void visualizzaVoti() {
+
+        r.registraValutazione("123", 8, "storia");
+        r.registraValutazione("123", 6, "storia");
+        r1.registraValutazione("345", 7, "storia");
+        r1.registraValutazione("345", 6, "geografia");
+
+        assertEquals(r.visualizzaVoti("123"),r1.visualizzaVoti("345"));
+    }
+    */
+    @Test
+    void alunniScarsi() {
+        r.registraValutazione("123", 8, "storia");
+        r.registraValutazione("123", 3, "storia");
+        r.registraValutazione("456", 5, "storia");
+        r.registraValutazione("456", 4, "storia");
+        r.calcoloMediaAlunno("123");
+        r.calcoloMediaAlunno("456");
+        assertEquals(2, r.alunniScarsi().count());
+    }
+
+    @Test
+    void alunniScarsiPerMateria() {
+        r.registraValutazione("123", 8, "storia");
+        r.registraValutazione("123", 3, "storia");
+        r.registraValutazione("456", 5, "storia");
+        r.registraValutazione("456", 4, "storia");
+        r.calcoloMediaAlunno("123");
+        r.calcoloMediaAlunno("456");
+        assertEquals(2, r.alunniScarsiPerMateria("storia").count());
+    }
+
+    @Test
+    void alunniPerfetti() {
+        r.registraValutazione("123", 8, "storia");
+        r.registraValutazione("123", 10, "storia");
+        r.registraValutazione("456", 9, "storia");
+        r.registraValutazione("456", 8, "storia");
+        r.calcoloMediaAlunno("123");
+        r.calcoloMediaAlunno("456");
+        assertEquals(2, r.alunniPerfetti().count());
+    }
+
+
+
 }
